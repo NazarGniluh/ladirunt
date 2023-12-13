3504a3d391230fa7f2d48536597dce9e8ada20cc
Anonymous <ostap.sukhomlyn@gmail.com>
2023-12-13 17:20:26 +0200

    ladirunt2

--- Character.py
+++ Character.py
@@ -3,10 +3,14 @@ class Character:
     def __init__(self, x, y, w, h, speed, texture):
         self.speed = speed
         self.texture = pygame.image.load(texture)
+        self.texture = pygame.transform.scale(self.texture, (w, h))
         self.hit_box = self.texture.get_rect()
         self.hit_box.x = x
         self.hit_box.y = y
-        self.texture = pygame.transform.scale(self.texture, (w, h))
+        self.sound_step = pygame.mixer.Sound("byistraya-hodba-po-betonnoy-poverhnosti-26128.ogg")
+        self.sound_step.play(-1)
+        self.sound_step.set_volume(200)
+
 
 
     def render(self, window):
@@ -14,11 +18,22 @@ class Character:
 
     def move(self):
         keys = pygame.key.get_pressed()
+        is_step = False
         if keys[pygame.K_d]:
             self.hit_box.x += self.speed
+            is_step = True
         if keys[pygame.K_w]:
             self.hit_box.y -= self.speed
+            is_step = True
         if keys[pygame.K_a]:
             self.hit_box.x -= self.speed
+            is_step = True
         if keys[pygame.K_s]:
             self.hit_box.y += self.speed
+            is_step = True
+
+
+        if is_step == True:
+            self.sound_step.set_volume(0.5)
+        else:
+            self.sound_step.set_volume(0)
--- main.py
+++ main.py
@@ -20,7 +20,7 @@ fps = pygame.time.Clock()
 fon = pygame.image.load('background.jpg')
 fon = pygame.transform.scale(fon, (800, 500))
 
-pacman = Character.Character(8, 295, 30, 20, 5, "hero.png" )
+pacman = Character.Character(400, 400, 30, 20, 5, "hero.png" )
 pacman2 = Enemy.Enemy(250, 350, 30, 20, 5, "cyborg.png", 100, 200, 300, 300)
 gold = Golden.Golden(220, 365, 50, 50,  "treasure.png")
 
@@ -39,9 +39,20 @@ Stin.append(Stinks(100, 415, 150, 10, (255, 255, 0)))
 Stin.append(Stinks(90, 325, 10, 100, (255, 255, 0)))
 Stin.append(Stinks(90, 325, 10, 100, (255, 255, 0)))
 Stin.append(Stinks(90, 325, 10, 100, (255, 255, 0)))
+Stin.append(Stinks(156, 178, 10, 100, (255, 255, 0)))
+Stin.append(Stinks(250, 415, 40, 10, (255, 255, 0)))
+Stin.append(Stinks(290, 365, 10, 60, (255, 255, 0)))
+Stin.append(Stinks(150, 318, 10, 59, (255, 255, 0)))
+Stin.append(Stinks(157, 367, 135, 10, (255, 255, 0)))
 
 
-while True:
+
+
+game = True
+
+money_sould = pygame.mixer.Sound("money.ogg")
+money_sould.play()
+while game:
 
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -57,6 +68,15 @@ while True:
 
     pacman.move()
     pacman2.move()
+
+    for Stinks in Stin:
+        if pacman.hit_box.colliderect(Stinks.rect):
+            game = False
+
+
+    if gold.hit_box.colliderect(pacman.hit_box):
+        money_sould.play()
+        gold.hit_box.x = 1000000
 
     window.fill((255,205,0))
     window.blit(fon, (0, 0))
